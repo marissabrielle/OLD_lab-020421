4ae4a7e27f36aa765e6700c5bbf77025002dc18a
Marissa Ade <marissabrielle@gmail.com>
2023-06-18 10:58:45 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// App: First JavaScript Application
+// Author: Marissa addEventListener
+// Date: 6/18/23
+
+//Allow the user to enter their guess
+let num = prompt('Guess a number between 1 and 10');
+
+//Check to see if the number is a winner
+if (num == 5) {
+
+    //If it is this, they got it right
+    alert('You guessed the right number!');
+} 
+
+else {
+    //If it isn't, they got it wrong
+    alert('You guessed the wrong number!');
+}
